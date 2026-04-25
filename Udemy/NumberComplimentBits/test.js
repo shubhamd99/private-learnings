533c344a2d78eede664e5e9b0da719e6dbe00261
shubhamd99 <shubhamdhage930@gmail.com>
2026-04-25 17:48:22 +0530

    udemy remove

--- Udemy/NumberComplimentBits/test.js
+++
@@ -1,9 +0,0 @@
-const findComplement = require('./index');
-
-test('findComplement function exists', () => {
-  expect(findComplement).toBeDefined();
-});
-
-test('findComplement', () => {
-    expect(findComplement(5)).toEqual(2);
-});
--- Udemy/RemoveCoveredIntervals/test.js
+++
@@ -1,9 +0,0 @@
-const removeCoveredIntervals = require('./index');
-
-test('removeCoveredIntervals function exists', () => {
-  expect(removeCoveredIntervals).toBeDefined();
-});
-
-test('removeCoveredIntervals', () => {
-    expect(removeCoveredIntervals([[1,4],[3,6],[2,8]])).toEqual(2);
-});
