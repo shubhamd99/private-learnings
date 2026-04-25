e887047ef11c2792f4c3d1ed21b2264dea201b3b
shubhamd99 <shubhamdhage930@gmail.com>
2026-04-25 17:50:10 +0530

    remove iterators

--- IteratorsAndGeneratorsInJS/iterators-generators-dragon-fun/make-dragon.js
+++
@@ -1,9 +0,0 @@
-const randomItem = require('./random-item');
-
-const makeDragon = () => {
-    const dragonSizes = ['big', 'small', 'medium'];
-    const dragonAbilities = ['fire', 'smoke', 'ice'];
-    return randomItem(dragonSizes) + ' ' + randomItem(dragonAbilities) + ' ' + 'dragon';
-}
-
-module.exports = makeDragon;
